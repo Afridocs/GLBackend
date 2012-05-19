548ef875e02bb1c8a4c1672d7cd9dfc4f6befa77
Arturo Filastò <hellais@torproject.org>
2012-05-19 21:05:20 +0200

    Implement REST interface (recursion is beauty) Start defining the ORM data model

---
+++ globaleaks/rest/api.py
@@ -0,0 +1,35 @@
+import json
+from twisted.web import resource
+
+#from globaleaks.tip import tipHandler
+#from globaleaks.targets import groupsHandler, targetsHandler
+#from globaleaks.stats import statsHandler
+#from globaleaks.admin import adminHandler
+from globaleaks.rest.handlers import *
+from globaleaks.rest.utils import processChildren
+
+class RESTful(resource.Resource):
+    API = {'node': nodeHandler,
+       'submission': submissionHandler,
+       'tip': tipHandler,
+       'admin': {'receivers': adminReceiversHandler,
+                 'config':  {'node': nodeConfigHandler,
+                             'delivery': deliveryConfigHandler,
+                             'storage': storageConfigHandler
+                            }
+                }
+    }
+
+    def __init__(self):
+        resource.Resource.__init__(self)
+        processChildren(self, self.API)
+
+    def getChild(self, path, request):
+        print path, request
+        return resource.Resource()
+
+if __name__ == "__main__":
+    from twisted.internet import reactor
+    from twisted.web import server
+    reactor.listenTCP(8082, server.Site(RESTful()))
+    reactor.run()
--- globaleaks/tip.py
+++ globaleaks/tip.py
@@ -1,28 +1,78 @@
-from twisted.web import resource
+from sqlalchemy import Table, Column, Integer, ForeignKey, String
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
 
-class tulipHandler(resource.Resource):
-    def __init__(self, path):
-        resource.Resource.__init__(self)
-        self.path = path
+Base = declarative_base()
 
-    def render_GET(self, request):
-        return "This is a tulip %s" % self.path
+class InternalTip(Base):
+    """
+    This is the internal representation of a Tip that has been submitted to the
+    GlobaLeaks node.
+    It has a one-to-many association with the individual Tips of every receiver
+    and whistleblower.
+    """
+    __tablename__ = 'internaltip'
 
+    id = Column(Integer, primary_key=True)
+    children = relationship("Tip", backref='internaltip')
 
-class tipHandler(resource.Resource):
-    def __init__(self, name="default"):
-        resource.Resource.__init__(self)
+    fields = Column(PickleType)
+    material = relationship("MaterialSet")
+    comments = Column(PickleType)
+    pertinence = Column(Integer)
+    expiration_time = Column(Date)
 
-    def render_GET(self, request):
-        return "Tips page.."
+class MaterialSet(Base):
+    """
+    This represents a material set: a collection of files.
+    """
+    __tablename__ = 'materialset'
+    id = Column(Integer, primary_key=True)
+    tip_id = Column(Integer, ForeignKey('internaltip.id'))
+    material = relationship("Material", backref='materialset')
 
-    def render_POST(self, request):
+    description = Column(String)
+
+class Material(Base):
+    """
+    Represents a material: a file.
+    """
+    __tablename__ = 'material'
+    id = Column(Integer, primary_key=True)
+    material_set_id = Column(Integer, ForeignKey('materialset.id'))
+
+    file_location = Column(String)
+    description = Column(String)
+
+class Tip(Base):
+    __tablename__ = 'tip'
+    id = Column(Integer, primary_key=True)
+    parent_id = Column(Integer, ForeignKey('internaltip.id'))
+    address = Column(String)
+    password = Column(String)
+
+    def add_comment(self, data):
         pass
 
-    def getChild(self, path, request):
-        return tulipHandler(path)
+class ReceiverTip(Tip):
+    total_view_count = Column(Integer)
+    total_download_count = Column(Integer)
+    relative_view_count = Column(Integer)
+    relative_download_count = Column(Integer)
 
+    def increment_visit(self):
+        pass
 
+    def increment_download(self):
+        pass
 
+    def delete_tulip(self):
+        pass
 
+    def download_material(self, id):
+        pass
+
+class WhistleblowerTip(Tip):
+    def add_material(self):
+        pass
 
