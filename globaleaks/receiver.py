0254c55315561c4197063c1c0bffcb5a81686c13
Arturo Filastò <arturo@filasto.net>
2012-08-23 13:53:19 +0200

    Break refactor branch

---
+++ globaleaks/receiver.py
@@ -0,0 +1,10 @@
+# -*- coding: UTF-8
+#   receiver
+#   ********
+#   :copyright: 2012 Hermes No Profit Association - GlobaLeaks Project
+#   :author: Arturo Filastò <art@globaleaks.org>
+#   :license: see LICENSE
+#
+from globaleaks import DummyHandler
+class Receiver(DummyHandler):
+    pass
